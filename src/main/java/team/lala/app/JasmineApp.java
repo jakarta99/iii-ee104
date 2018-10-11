cd291682009cd99f1017e97b202a026d5f56e02e
ASUS <ASUS@DESKTOP-GRH6OVN>
2018-10-11 19:31:41 +0800

    10/11

---
+++ src/main/java/team/lala/app/JasmineApp.java
@@ -0,0 +1,10 @@
+package team.lala.app;
+
+public class JasmineApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jasmine 12345");
+	}
+
+}
