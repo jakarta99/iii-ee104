0dc01b36757c9b5c378c9c395b8e57b5c5ba8b3f
ASUS <ASUS@DESKTOP-GRH6OVN>
2018-10-11 20:05:09 +0800

    123

--- src/main/java/team/lala/jasmine/service/calculateServicImpl.java
+++ src/main/java/team/lala/jasmine/service/calculateServicImpl.java
@@ -17,7 +17,7 @@ public class calculateServicImpl implements CalculateService {
 
 	@Override
 	public int mul(int a, int b) {
-		return a;
+		return a * b;
 	}
 
 	public static void main(String[] args) {
