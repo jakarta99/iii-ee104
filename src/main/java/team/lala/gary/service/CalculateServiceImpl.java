9ae569ff31edc803ed12c53bfae2e1351a9a1158
GaryLee <jakarta99@gmail.com>
2018-10-11 20:34:23 +0800

    first junit testing.

--- src/main/java/team/lala/gary/service/CalculateServiceImpl.java
+++ src/main/java/team/lala/gary/service/CalculateServiceImpl.java
@@ -6,20 +6,17 @@ public class CalculateServiceImpl implements CalculateService {
 
 	@Override
 	public int add(int a, int b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return a + b;
 	}
 
 	@Override
 	public int sub(int a, int b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return a - b;
 	}
 
 	@Override
 	public int mul(int a, int b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return a * b;
 	}
 
 }
---
+++ src/test/java/team/lala/service/CalculateServiceTest.java
@@ -0,0 +1,38 @@
+package team.lala.service;
+
+import org.junit.Test;
+
+import team.lala.gary.service.CalculateServiceImpl;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculateServiceTest {
+
+	@Test
+	public void testAdd() {
+		CalculateService calculateService = new CalculateServiceImpl();
+		int sum = calculateService.add(1, 2);
+		
+		assertEquals(3, sum);
+	}
+	
+	@Test
+	public void testSub() {
+		CalculateService calculateService = new CalculateServiceImpl();
+		int result = calculateService.sub(3, 1);
+		
+		assertEquals(2, result);
+	}
+	
+	@Test
+	public void testMul() {
+		CalculateService calculateService = new CalculateServiceImpl();
+		int result = calculateService.mul(3, 2);
+		
+		assertEquals(6, result);
+	}
+	
+	
+	
+
+}
