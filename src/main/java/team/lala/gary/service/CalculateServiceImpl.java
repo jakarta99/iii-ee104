c81579c65da9b43c16be723337fe1070fb617080
GaryLee <jakarta99@gmail.com>
2018-10-11 19:50:36 +0800

    CalculateService

---
+++ src/main/java/team/lala/gary/service/CalculateServiceImpl.java
@@ -0,0 +1,25 @@
+package team.lala.gary.service;
+
+import team.lala.service.CalculateService;
+
+public class CalculateServiceImpl implements CalculateService {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/team/lala/service/CalculateService.java
@@ -0,0 +1,12 @@
+package team.lala.service;
+
+public interface CalculateService {
+
+	public int add(int a, int b);
+	
+	public int sub(int a, int b);
+	
+	public int mul(int a, int b);
+	
+	
+}
