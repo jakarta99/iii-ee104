2ad5d690ae1fd132103ee61d1cc3386a40c817df
userr <userr@user>
2018-10-11 20:47:57 +0800

    roy

--- src/main/java/team/lala/roy/service/CalculateServiceImpl.java
+++ src/main/java/team/lala/roy/service/CalculateServiceImpl.java
@@ -1,24 +1,25 @@
 package team.lala.roy.service;
+
 import team.lala.service.CalculateService;
 
-public class CalculateServiceImpl implements CalculateService{
+public class CalculateServiceImpl implements CalculateService {
 
-		@Override
-		public int add(int a, int b) {
+	@Override
+	public int add(int a, int b) {
 
-			return a + b ;
-		}
+		return a + b;
+	}
 
-		@Override
-		public int sub(int a, int b) {
+	@Override
+	public int sub(int a, int b) {
 
-			return a - b ;
-		}
+		return a - b;
+	}
 
-		@Override
-		public int mul(int a, int b) {
+	@Override
+	public int mul(int a, int b) {
 
-			return a * b ;
-		}
+		return a * b;
+	}
 
 }
