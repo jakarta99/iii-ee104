0c741aef405725fae8ff17b8c1ba14e351026c7a
ASUS <ASUS@DESKTOP-GRH6OVN>
2018-10-11 19:59:45 +0800

    123

---
+++ src/main/java/team/lala/jasmine/service/calculateServicImpl.java
@@ -0,0 +1,30 @@
+package team.lala.jasmine.service;
+
+import team.lala.service.CalculateService;
+
+public class calculateServicImpl implements CalculateService {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
