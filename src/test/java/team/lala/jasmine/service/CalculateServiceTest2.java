9c9fd758f5cbae93a598bf6328c376d914faf4c5
User <User@EEIT-PC>
2018-10-16 15:31:09 +0800

    CalculateServiceTest version2

--- src/test/java/team/lala/jasmine/service/CalculateServiceTest2.java
+++ src/test/java/team/lala/jasmine/service/CalculateServiceTest2.java
@@ -7,8 +7,25 @@ import org.junit.Test;
 public class CalculateServiceTest2 {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() {
+		CalculateServiceImpl a = new CalculateServiceImpl();
+		assertEquals(3, a.add(1, 2));
+		assertEquals(8, a.add(3, 5));
+	}
+	
+	@Test
+	public void testSub() {
+		CalculateServiceImpl a = new CalculateServiceImpl();
+		assertEquals(8, a.sub(10, 2));
+		assertEquals(11, a.sub(99, 88));
+	}
+	
+	@Test
+	public void testMul(){
+		CalculateServiceImpl a = new CalculateServiceImpl();
+		assertEquals(30, a.mul(5, 6));
+		assertEquals(56, a.mul(8, 7));
+		
 	}
 
 }
