f3d9b91c99af8e314277eed018e724908c11e4bf
User <User@EEIT-PC>
2018-10-16 15:47:49 +0800

    1

--- src/test/java/team/lala/brian/service/CalculateServiceTest.java
+++ src/test/java/team/lala/brian/service/CalculateServiceTest.java
@@ -2,7 +2,29 @@ package team.lala.brian.service;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class CalculateServiceTest {
 	
+	@Test
+	public void testAdd() {
+		CalculateServiceImpl calculateService = new CalculateServiceImpl();
+		assertEquals(3, calculateService.add(1, 2));
+		assertEquals(9, calculateService.add(4, 5));
+	}
+
+	@Test
+	public void testSub() {
+		CalculateServiceImpl calculateService = new CalculateServiceImpl();
+		assertEquals(1, calculateService.sub(3, 2));
+		assertEquals(2, calculateService.sub(6, 4));
+	}
+
+	@Test
+	public void testMul() {
+		CalculateServiceImpl calculateService = new CalculateServiceImpl();
+		assertEquals(2, calculateService.mul(1, 2));
+		assertEquals(12, calculateService.mul(3, 4));
+	}
 
 }
