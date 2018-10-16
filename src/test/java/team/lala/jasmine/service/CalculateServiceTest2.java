a13e609f19b393033e6743221959cff205fed78d
User <User@EEIT-PC>
2018-10-16 14:43:28 +0800

    practice

---
+++ src/test/java/team/lala/jasmine/service/CalculateServiceTest2.java
@@ -0,0 +1,14 @@
+package team.lala.jasmine.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculateServiceTest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
