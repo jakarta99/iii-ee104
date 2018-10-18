355781d33a029fe4c6dfdc3b09e6d4d59c80eb12
GaryLee <jakarta99@gmail.com>
2018-10-18 20:51:20 +0800

    Demo sample

--- src/main/java/team/lala/timebank/TimeBankApp.java
+++ src/main/java/team/lala/timebank/TimeBankApp.java
@@ -12,17 +12,23 @@ public class TimeBankApp {
 		Long memberId = 1L;
 		
 		TimeLedgerService timeLedgerService;
-		// 
+		// 1. List my ledgers 
 		Collection<TimeLedger> timeLedgers = timeLedgerService.getByMemberId(memberId);
 		for(TimeLedger timeLedger : timeLedgers) {
 			System.out.println(timeLedger); // @asdf124
 		}
 		
-		// Choose one Service from Service List
+		// 2. Show Job List
+		JobService.getAll();
+		forEach List;
 		
 		
-		// After verified, Deposit service hours
-		timeLedgerService.deposit(service.getHours(), memberId);
+		// 3. Choose one Job (id:1) from Job List
+		JobService.getById(1);
+		
+		
+		// 4. After verified, Deposit service hours
+		timeLedgerService.deposit(job.getHours(), memberId);
 		
 
 	}
