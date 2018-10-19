76209e40f71e16e1a9bec2d880a32a14c0d5de75
User <User@EEIT-PC>
2018-10-19 17:58:08 +0800

    11

--- src/main/java/team/lala/timebank/entity/Job.java
+++ src/main/java/team/lala/timebank/entity/Job.java
@@ -3,8 +3,7 @@ package team.lala.timebank.entity;
 import java.time.LocalDateTime;
 
 public class Job {
-	private long jobId;//工作ID
-
+	
 	private String requester; // 刊登者
 
 	private LocalDateTime postedDay;// po文&徵人日期
@@ -24,6 +23,100 @@ public class Job {
 	private String contactTel;// 聯絡電話
 
 	private String contactEmail;// 聯絡信箱
+	
+	
+	
+	
+	
+	private long jobId;//工作ID
+
+	public long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(long jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getRequester() {
+		return requester;
+	}
+
+	public void setRequester(String requester) {
+		this.requester = requester;
+	}
+
+	public LocalDateTime getPostedDay() {
+		return postedDay;
+	}
+
+	public void setPostedDay(LocalDateTime postedDay) {
+		this.postedDay = postedDay;
+	}
+
+	public LocalDateTime getPostedEndDay() {
+		return postedEndDay;
+	}
+
+	public void setPostedEndDay(LocalDateTime postedEndDay) {
+		this.postedEndDay = postedEndDay;
+	}
+
+	public LocalDateTime getJobStartDay() {
+		return jobStartDay;
+	}
+
+	public void setJobStartDay(LocalDateTime jobStartDay) {
+		this.jobStartDay = jobStartDay;
+	}
+
+	public LocalDateTime getJobEndDay() {
+		return jobEndDay;
+	}
+
+	public void setJobEndDay(LocalDateTime jobEndDay) {
+		this.jobEndDay = jobEndDay;
+	}
+
+	public String getJobLocation() {
+		return jobLocation;
+	}
+
+	public void setJobLocation(String jobLocation) {
+		this.jobLocation = jobLocation;
+	}
+
+	public String getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+
+	public String getContactPerson() {
+		return contactPerson;
+	}
+
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+
+	public String getContactTel() {
+		return contactTel;
+	}
+
+	public void setContactTel(String contactTel) {
+		this.contactTel = contactTel;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
 	
 	
 
