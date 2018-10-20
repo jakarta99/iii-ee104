375df48bc987dfa1af323633485277bc38beceb6
Brian <Brian@LAPTOP-19936VFA>
2018-10-20 14:09:46 +0800

    add tostring

--- src/main/java/team/lala/timebank/entity/Job.java
+++ src/main/java/team/lala/timebank/entity/Job.java
@@ -3,6 +3,7 @@ package team.lala.timebank.entity;
 import java.time.LocalDateTime;
 
 public class Job {
+	private long jobId;// 工作ID
 
 	private String requester; // 刊登者
 
@@ -10,25 +11,23 @@ public class Job {
 
 	private int timeValue;// 可獲得時數(工作的時間)
 
-	private LocalDateTime postedDay;// po文&徵人日期
-
-	private LocalDateTime postedEndDay;// 徵人結束文日期
-
-	private LocalDateTime jobStartDay;// 工作開始日期
-
-	private LocalDateTime jobEndDay;// 工作結束日期
-
-	private String jobLocation;// 工作地點
-
-	private String serviceType;// 服務類別
-
-	private String contactPerson;// 聯絡人
-
-	private String contactTel;// 聯絡電話
-
-	private String contactEmail;// 聯絡信箱
-
-	private long jobId;// 工作ID
+//	private LocalDateTime postedDay;// po文&徵人日期
+//
+//	private LocalDateTime postedEndDay;// 徵人結束文日期
+//
+//	private LocalDateTime jobStartDay;// 工作開始日期
+//
+//	private LocalDateTime jobEndDay;// 工作結束日期
+//
+//	private String jobLocation;// 工作地點
+//
+//	private String serviceType;// 服務類別
+//
+//	private String contactPerson;// 聯絡人
+//
+//	private String contactTel;// 聯絡電話
+//
+//	private String contactEmail;// 聯絡信箱
 
 	public long getJobId() {
 		return jobId;
@@ -46,78 +45,6 @@ public class Job {
 		this.requester = requester;
 	}
 
-	public LocalDateTime getPostedDay() {
-		return postedDay;
-	}
-
-	public void setPostedDay(LocalDateTime postedDay) {
-		this.postedDay = postedDay;
-	}
-
-	public LocalDateTime getPostedEndDay() {
-		return postedEndDay;
-	}
-
-	public void setPostedEndDay(LocalDateTime postedEndDay) {
-		this.postedEndDay = postedEndDay;
-	}
-
-	public LocalDateTime getJobStartDay() {
-		return jobStartDay;
-	}
-
-	public void setJobStartDay(LocalDateTime jobStartDay) {
-		this.jobStartDay = jobStartDay;
-	}
-
-	public LocalDateTime getJobEndDay() {
-		return jobEndDay;
-	}
-
-	public void setJobEndDay(LocalDateTime jobEndDay) {
-		this.jobEndDay = jobEndDay;
-	}
-
-	public String getJobLocation() {
-		return jobLocation;
-	}
-
-	public void setJobLocation(String jobLocation) {
-		this.jobLocation = jobLocation;
-	}
-
-	public String getServiceType() {
-		return serviceType;
-	}
-
-	public void setServiceType(String serviceType) {
-		this.serviceType = serviceType;
-	}
-
-	public String getContactPerson() {
-		return contactPerson;
-	}
-
-	public void setContactPerson(String contactPerson) {
-		this.contactPerson = contactPerson;
-	}
-
-	public String getContactTel() {
-		return contactTel;
-	}
-
-	public void setContactTel(String contactTel) {
-		this.contactTel = contactTel;
-	}
-
-	public String getContactEmail() {
-		return contactEmail;
-	}
-
-	public void setContactEmail(String contactEmail) {
-		this.contactEmail = contactEmail;
-	}
-
 	public String getJobTitle() {
 		return jobTitle;
 	}
@@ -133,5 +60,84 @@ public class Job {
 	public void setTimeValue(int timeValue) {
 		this.timeValue = timeValue;
 	}
+
+	@Override
+	public String toString() {
+
+		return "Job [jobId=" + jobId + ", requester=" + requester + ", jobTitle=" + jobTitle + ", timeValue="
+				+ timeValue + "]";
+	}
+
+//	public LocalDateTime getPostedDay() {
+//		return postedDay;
+//	}
+//
+//	public void setPostedDay(LocalDateTime postedDay) {
+//		this.postedDay = postedDay;
+//	}
+//
+//	public LocalDateTime getPostedEndDay() {
+//		return postedEndDay;
+//	}
+//
+//	public void setPostedEndDay(LocalDateTime postedEndDay) {
+//		this.postedEndDay = postedEndDay;
+//	}
+//
+//	public LocalDateTime getJobStartDay() {
+//		return jobStartDay;
+//	}
+//
+//	public void setJobStartDay(LocalDateTime jobStartDay) {
+//		this.jobStartDay = jobStartDay;
+//	}
+//
+//	public LocalDateTime getJobEndDay() {
+//		return jobEndDay;
+//	}
+//
+//	public void setJobEndDay(LocalDateTime jobEndDay) {
+//		this.jobEndDay = jobEndDay;
+//	}
+//
+//	public String getJobLocation() {
+//		return jobLocation;
+//	}
+//
+//	public void setJobLocation(String jobLocation) {
+//		this.jobLocation = jobLocation;
+//	}
+//
+//	public String getServiceType() {
+//		return serviceType;
+//	}
+//
+//	public void setServiceType(String serviceType) {
+//		this.serviceType = serviceType;
+//	}
+//
+//	public String getContactPerson() {
+//		return contactPerson;
+//	}
+//
+//	public void setContactPerson(String contactPerson) {
+//		this.contactPerson = contactPerson;
+//	}
+//
+//	public String getContactTel() {
+//		return contactTel;
+//	}
+//
+//	public void setContactTel(String contactTel) {
+//		this.contactTel = contactTel;
+//	}
+//
+//	public String getContactEmail() {
+//		return contactEmail;
+//	}
+//
+//	public void setContactEmail(String contactEmail) {
+//		this.contactEmail = contactEmail;
+//	}
 
 }
