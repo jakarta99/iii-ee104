611c08fc25ea55068990036603b903eba9d7f38e
Roy <z403040473@gmail.com>
2018-10-21 10:39:26 +0800

    member service

--- src/main/java/team/lala/timebank/service/MemberService.java
+++ src/main/java/team/lala/timebank/service/MemberService.java
@@ -1,18 +1,76 @@
 package team.lala.timebank.service;
 
+import java.util.Collection;
+
 import team.lala.timebank.dao.MemberDao;
 import team.lala.timebank.entity.Member;
 
 public class MemberService {
 
 	private MemberDao memberDao = new MemberDao();
-	
+
+	/**
+	 * insert member
+	 * 
+	 * @param member
+	 */
 	public void insert(Member member) {
-		
+		memberDao.getConnection();
 		memberDao.insert(member);
-		
-		
+		memberDao.closeConnection();
 	}
-	
-	
+
+	/**
+	 * update member
+	 * 
+	 * @param member
+	 */
+	public void update(Member member) {
+		memberDao.getConnection();
+		memberDao.update(member);
+		memberDao.closeConnection();
+	}
+
+	/**
+	 * find member by id
+	 * 
+	 * @param id
+	 * @return member
+	 */
+	public Member findMemberById(Long id) {
+		memberDao.getConnection();
+		Member member = memberDao.findOne(id);
+		memberDao.closeConnection();
+		if (member == null) {
+			System.out.println("memberId does not exist");
+		}
+		return member;
+	}
+
+	/**
+	 * lists all the members
+	 * 
+	 * @return the collection of all members
+	 */
+	public Collection<Member> findAll() {
+		memberDao.getConnection();
+		Collection<Member> memberCollection = memberDao.findAll();
+		memberDao.closeConnection();
+		if (memberCollection == null) {
+			System.out.println("memberCollection does not exist");
+		}
+		return memberCollection;
+	}
+
+	/**
+	 * delete member by id
+	 * 
+	 * @param id
+	 */
+	public void delete(Long id) {
+		memberDao.getConnection();
+		memberDao.delete(id);
+		memberDao.closeConnection();
+	}
+
 }
