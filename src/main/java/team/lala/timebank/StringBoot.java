be8442bcb0ca06227c7560425a09e313c6aacd83
GaryLee <jakarta99@gmail.com>
2018-10-23 19:46:55 +0800

    initialize timbank sample

--- src/main/java/team/lala/timebank/StringBoot.java
+++ src/main/java/team/lala/timebank/StringBoot.java
@@ -1,15 +1,32 @@
 package team.lala.timebank;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import team.lala.timebank.service.CalculateService;
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class StringBoot {
 
+	@Autowired
+	private CalculateService calculateService;
+	
+	
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		
+		
+		
+		String result = "<html><h1>Hello World!</h1></html>";
+		
+		int data = calculateService.add(1,4);
+		
+		result += data;
+		
+		return result;
 	}
 
 	public static void main(String[] args) throws Exception {
---
+++ src/main/java/team/lala/timebank/service/CalculateService.java
@@ -0,0 +1,13 @@
+package team.lala.timebank.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculateService {
+
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+}
