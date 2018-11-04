d2a61e3b7eb77baf15c2d1dec2007dea72082b90
Jasmine <jasminetseng0427@gmail.com>
2018-11-04 16:01:57 +0800

    1.新增註解。 2.提款測試:新增「無交易紀錄之會員提款」之測試   3.新增testUpdate()

--- src/test/java/team/lala/timebank/service/TimeLedgerServiceTest.java
+++ src/test/java/team/lala/timebank/service/TimeLedgerServiceTest.java
@@ -12,48 +12,52 @@ public class TimeLedgerServiceTest {
 
 	private TimeLedgerService timeLedgerService = new TimeLedgerService();
 
-	
+	//存款測試
 	@Test
 	public void testDeposit() {
-         //正常存款
-		System.out.println(timeLedgerService.searchLastTransaction(2L).getBalanceValue());
+        //過去已有交易紀錄之會員
+		System.out.println("存款前記錄=" + timeLedgerService.searchLastTransaction(2L).toString());
 		timeLedgerService.deposit(2, 2L);
-		System.out.println(timeLedgerService.searchLastTransaction(2L).toString());
+		System.out.println("存款後記錄=" + timeLedgerService.searchLastTransaction(2L).toString());
 		
-        //第一次存款
-		System.out.println(timeLedgerService.searchLastTransaction(3L).toString());
+        //第一次存款之會員
+		System.out.println("存款前記錄=" + timeLedgerService.searchLastTransaction(3L).toString());
 		timeLedgerService.deposit(20, 3L);
-		System.out.println(timeLedgerService.searchLastTransaction(3L).toString());
+		System.out.println("存款後記錄=" + timeLedgerService.searchLastTransaction(3L).toString());
 
 	}
-/*
+
+	//提款測試
 	@Test
 	public void testWithdrawal() {
-        //正常提款
+        //餘額足夠提款
 		timeLedgerService.withdrawal(3, 2L);
-		
-		
+
         //超額提款
 		timeLedgerService.withdrawal(3000, 2L);
+		
+		//從無交易紀錄之會員提款
+		timeLedgerService.withdrawal(3000, 10L);
 	}
 	
 	@Test
 	public void testUpdate() {
 	
 	}
+	
 	@Test
 	public void testDelete() {
 		
 	}
 
 	@Test
-	public void testSearchALLTransaction() {
-		Collection<TimeLedger> timeLedgers = timeLedgerService.searchALLTransaction(1L);
+	public void testGetAll() {
+		Collection<TimeLedger> timeLedgers = timeLedgerService.getAll(1L);
 		for(TimeLedger timeLedger : timeLedgers) {
 			System.out.println(timeLedger.toString());
 		}
 		
-		timeLedgerService.searchALLTransaction(10L);
+		timeLedgerService.getAll(10L);
 	}
 
 
@@ -67,5 +71,5 @@ public class TimeLedgerServiceTest {
 		timeLedgerService.searchLastTransaction(10L);
 		
 	}
-*/
+
 }
