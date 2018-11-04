b2999364ef6fff1d98274997f6819aed3327cfc6
louis <louisfffflouisqqqq@gmail.com>
2018-11-04 18:14:38 +0800

    delete penalty

--- src/main/java/team/lala/timebank/dao/PenaltyDao.java
+++
@@ -1,9 +0,0 @@
-package team.lala.timebank.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import team.lala.timebank.entity.penalty;
-
-public interface PenaltyDao extends JpaRepository<penalty, Long> {
-
-}
