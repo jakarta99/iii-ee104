a84f392cb8aca7a47349988367444d393a072d1a
Jasmine <jasminetseng0427@gmail.com>
2018-11-04 15:36:29 +0800

    add method:findTop1ByMemberIdOrderByTransactionTime、findByMemberIdAndTransactionTimeBetween 改TimeLedgerService邏輯:查無資料不代表會員不存在，而是會員尚無交易紀錄。controller需要先判斷會員id存在後，才呼叫TimeLedgerService。

--- src/main/java/team/lala/timebank/service/TimeLedgerService.java
+++ src/main/java/team/lala/timebank/service/TimeLedgerService.java
@@ -11,121 +11,100 @@ import team.lala.timebank.entity.TimeLedger;
 
 @Service
 public class TimeLedgerService {
-	
+
 	@Autowired
 	private TimeLedgerDao timeLedgerDao;
 
-	
 	// 存款
 	public void deposit(int hours, Long memberId) {
-		
+		// 1.找出會員最近一筆交易紀錄
+		TimeLedger lastTimeLedger = timeLedgerDao.findTop1ByMemberIdOrderByTransactionTime(memberId);
 
-		// 1. findTimeLedgerByMemberId
-//		TimeLedger lastTimeLedger = timeLedgerDao.findTop1ByMemberIdOrderByTransactionTime(memberId);
-//		if (lastTimeLedger == null) {
-//			lastTimeLedger = new TimeLedger();
-//			System.out.println("開戶成功");
-//		}
+		// 尚無交易紀錄者，先將存款餘額設為0
+		if (lastTimeLedger == null) {
+			lastTimeLedger = new TimeLedger();
+			lastTimeLedger.setBalanceValue(0); // balanceValue型別為Integer，預設null改為0
+			System.out.println("會員第一次交易");
+		}
 
 		// 2. prepare timeledger data
-//		TimeLedger timeLedger = new TimeLedger();
-//		timeLedger.setMemberId(memberId);
-//		timeLedger.setDepositValue(hours);
-//		timeLedger.setBalanceValue(lastTimeLedger.getBalanceValue() + hours);
-//		timeLedger.setTransactionTime(LocalDateTime.now());
-//		timeLedger.setDescription("deposit");
+		TimeLedger timeLedger = new TimeLedger();
+		timeLedger.setMemberId(memberId);
+		timeLedger.setDepositValue(hours);
+		timeLedger.setBalanceValue(lastTimeLedger.getBalanceValue() + hours);
+		timeLedger.setTransactionTime(LocalDateTime.now());
+		timeLedger.setDiscription("deposit");
 
 		// 3. insert
-//		timeLedgerDao.insert(timeLedger);
-//		System.out.println("存款成功");
-//		timeLedgerDao.closeConnection();
-
+		timeLedgerDao.save(timeLedger);
+		System.out.println("存款成功");
 	}
 
 	// 提款
-//	public void withdrawal(int hours, Long memberId) {
-//		timeLedgerDao.getConnection();
-//
-//		// 1. findTimeLedgerByMemberId
-//		TimeLedger lastTimeLedger = timeLedgerDao.findTop1ByMemberIdOrderByTransactionTime(memberId);
-//		if (lastTimeLedger.getBalanceValue() < hours) {
-//			System.out.println("餘額不足");
-//
-//		} else {
-//
-//			// 2. prepare timeledger data
-//			TimeLedger timeLedger = new TimeLedger();
-//			timeLedger.setMemberId(memberId);
-//			timeLedger.setWithdrawlValue(hours);
-//			timeLedger.setTransactionTime(LocalDateTime.now());
-//			timeLedger.setBalanceValue(lastTimeLedger.getBalanceValue() - hours);
-//			timeLedger.setDescription("withdrawal");
-//
-//			// 3. insert
-//			timeLedgerDao.insert(timeLedger);
-//			System.out.println("提款成功");
-//		}
-//		timeLedgerDao.closeConnection();
-//
-//	}
+	public void withdrawal(Integer hours, Long memberId) {
+		// 1. findTimeLedgerByMemberId
+		TimeLedger lastTimeLedger = timeLedgerDao.findTop1ByMemberIdOrderByTransactionTime(memberId);
 
-	/*
-	 * 查詢某段時間的交易紀錄 public void searchOneTransactionByTime(LocalDateTime
-	 * transactionTimeBegin, LocalDateTime transactionTimeEnd) {
-	 * timeLedgerDao.getConnection();
-	 * 
-	 * Collection<TimeLedger> periodMemberTimeLedgers =
-	 * timeLedgerDao.findByPeriodTime(transactionTimeBegin, transactionTimeEnd); if
-	 * (periodMemberTimeLedgers == null) { System.out.println("查無此會員"); }else{ for
-	 * (TimeLedger timeLedger : periodMemberTimeLedgers) {
-	 * System.out.print(timeLedger.getId());
-	 * System.out.print(timeLedger.getMemberId());
-	 * System.out.print(timeLedger.getTransactionTime());
-	 * System.out.print(timeLedger.getDepositValue());
-	 * System.out.print(timeLedger.getWithdrawlValue());
-	 * System.out.print(timeLedger.getBalanceValue());
-	 * System.out.println(timeLedger.getDescription());
-	 * 
-	 * } } timeLedgerDao.closeConnection(); }
-	 */
+		// 尚無交易紀錄，或帳戶餘額小於本次提款時數者，不得提款
+		if (lastTimeLedger == null || lastTimeLedger.getBalanceValue() < hours) {
+			System.out.println("餘額不足，不得提款");
+		} else {
+			// 2. prepare timeledger data
+			TimeLedger timeLedger = new TimeLedger();
+			timeLedger.setMemberId(memberId);
+			timeLedger.setWithdrawalValue(hours);
+			timeLedger.setTransactionTime(LocalDateTime.now());
+			timeLedger.setBalanceValue(lastTimeLedger.getBalanceValue() - hours);
+			timeLedger.setDiscription("withdrawal");
+
+			// 3. insert
+			timeLedgerDao.save(timeLedger);
+			System.out.println("提款成功");
+		}
+
+	}
+
+	// 查詢特定會員某段時間的交易紀錄
+	public void searchTransactionByMemberIdAndTime(Long memberId, LocalDateTime transactionTimeBegin,
+			LocalDateTime transactionTimeEnd) {
+		Collection<TimeLedger> periodSpecificMemberTimeLedgers = timeLedgerDao
+				.findByMemberIdAndTransactionTimeBetween(memberId, transactionTimeBegin, transactionTimeEnd);
+		if (periodSpecificMemberTimeLedgers.isEmpty()) {
+			System.out.println("此區間查無交易紀錄");
+		} else {
+			for (TimeLedger timeLedger : periodSpecificMemberTimeLedgers) {
+				System.out.print(timeLedger.toString());
+			}
+		}
+	}
 
 	// 更新一筆資料
 
 	public void update(TimeLedger timeLedger) {
 		timeLedgerDao.save(timeLedger);
-		
-		System.out.println("更新成功");
+		System.out.println("更新單筆存摺資料成功");
 	}
 
 	// 刪除一筆資料
 	public void delete(Long id) {
-		
 		timeLedgerDao.deleteById(id);
-		System.out.println("刪除成功");
-		
+		System.out.println("刪除單筆存摺資料成功");
 	}
 
 	// 查詢全部交易紀錄
 	public Collection<TimeLedger> getAll(Long memberId) {
-		
-
 		Collection<TimeLedger> specificMemberTimeLedgers = timeLedgerDao.findAll();
-		
-		
 		return specificMemberTimeLedgers;
 	}
 
 	// 尋找最新一筆資料
-//	public TimeLedger searchLastTransaction(Long memberId) {
-//		
-//
-////		TimeLedger timeLedger = timeLedgerDao.findTop1ByMemberIdOrderByTransactionTime(memberId);
-//		if (timeLedger == null) {
-//			System.out.println("查無此會員");
-//		}
-//
-//		
-//		return timeLedger;
-//	}
+	public TimeLedger searchLastTransaction(Long memberId) {
+		TimeLedger timeLedger = timeLedgerDao.findTop1ByMemberIdOrderByTransactionTime(memberId);
+		// 尚無交易紀錄者
+		if (timeLedger == null) {
+			System.out.println("會員尚無交易紀錄");
+		}
+		return timeLedger;
+	}
 
 }
