566328e99fd3c381b3c67e73da11d12f1ce7c3e3
louis <louisfffflouisqqqq@gmail.com>
2018-11-06 22:41:59 +0800

    penalty

--- src/main/java/team/lala/timebank/dao/PenaltyDao.java
+++ src/main/java/team/lala/timebank/dao/PenaltyDao.java
@@ -2,8 +2,8 @@ package team.lala.timebank.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import team.lala.timebank.entity.penalty;
+import team.lala.timebank.entity.Penalty;
 
-public interface PenaltyDao extends JpaRepository<penalty, Long> {
+public interface PenaltyDao extends JpaRepository<Penalty, Long> {
 
 }
