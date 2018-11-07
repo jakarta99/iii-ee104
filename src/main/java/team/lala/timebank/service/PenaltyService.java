ff6fc3a4e82d4fb9b89bb4ebe07365f019c3bc1f
louis <louisfffflouisqqqq@gmail.com>
2018-11-07 17:14:36 +0800

    penalty

--- src/main/java/team/lala/timebank/service/PenaltyService.java
+++ src/main/java/team/lala/timebank/service/PenaltyService.java
@@ -1,14 +1,29 @@
-package team.lala.timebank;
+package team.lala.timebank.service;
 
+import java.util.List;
+import team.lala.timebank.dao.PenaltyDao;
+import team.lala.timebank.entity.Penalty;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PenaltyService {
-	
-	private PenaltyDao penaltydao ;
-	
-	public void insert(Penalty penalty) {
-		
+
+	@Autowired
+	private PenaltyDao penaltyDao;
+
+	public void update(Penalty penalty) {
+		penaltyDao.save(penalty);
 	}
-	
-	public void update() {
+
+	public List<Penalty> findAll(long id) {
+		List<Penalty> specificMemberPenalty = penaltyDao.findAll();
+		return specificMemberPenalty;
+	}
+
+	public void delete(Long penaltyListId) {
+		penaltyDao.deleteById(penaltyListId);
 		
 	}
 
