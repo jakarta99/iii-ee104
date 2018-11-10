bd81d14eb21da817846744d6a5997eb7deedf5e1
Jasmine <jasminetseng0427@gmail.com>
2018-11-10 18:51:16 +0800

    adjust parameter name

--- src/main/java/team/lala/timebank/dao/TimeLedgerDao.java
+++ src/main/java/team/lala/timebank/dao/TimeLedgerDao.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.lala.timebank.entity.TimeLedger;
 
 public interface TimeLedgerDao extends JpaRepository<TimeLedger, Long> {
-	//查詢特定會員最近一筆交易紀錄
+	//查詢特定會員最近一筆交易紀錄(jasmine)
 	public TimeLedger findTop1ByMemberIdOrderByTransactionTime(Long memberId);
 	
-	//查詢特定會員特定區間之交易紀錄
+	//查詢特定會員特定區間之交易紀錄(jasmine)
 	public Collection<TimeLedger> findByMemberIdAndTransactionTimeBetween(Long memberId,
 			LocalDateTime transactionTimeBegin, LocalDateTime transactionTimeEnd);
 }
