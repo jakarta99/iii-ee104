1af6a0f80b258d229e2b49854050411d24f141ab
Jasmine <jasminetseng0427@gmail.com>
2018-11-11 16:39:37 +0800

    modify method:findTop1ByMemberIdOrderByTransactionTime()>>>findTop1ByMemberIdOrderByTransactionTimeDesc()

--- src/main/java/team/lala/timebank/dao/TimeLedgerDao.java
+++ src/main/java/team/lala/timebank/dao/TimeLedgerDao.java
@@ -9,7 +9,7 @@ import team.lala.timebank.entity.TimeLedger;
 
 public interface TimeLedgerDao extends JpaRepository<TimeLedger, Long> {
 	//查詢特定會員最近一筆交易紀錄(jasmine)
-	public TimeLedger findTop1ByMemberIdOrderByTransactionTime(Long memberId);
+	public TimeLedger findTop1ByMemberIdOrderByTransactionTimeDesc(Long memberId);
 	
 	//查詢特定會員特定區間之交易紀錄(jasmine)
 	public Collection<TimeLedger> findByMemberIdAndTransactionTimeBetween(Long memberId,
