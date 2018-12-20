ce37b3d493c0333183c349c1a0ef90fc81c62f53
Sheila <sheila06048@gmail.com>
2018-12-20 17:56:15 +0800

    modify security user list

--- src/main/java/team/lala/timebank/web/AuthController.java
+++ src/main/java/team/lala/timebank/web/AuthController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
-
-//	@GetMapping("/login")
-//	public String loginPage() {
-//		return "/index";
-//	}
 	
 	@GetMapping("/login")
 	public String loginPage() {
