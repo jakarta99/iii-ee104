c92eab730c3ab0d5ed09d395d0046bf0404d0f53
Sheila <sheila06048@gmail.com>
2018-12-20 19:06:00 +0800

    add password encoder

--- src/main/java/team/lala/timebank/config/SecurityConfig.java
+++ src/main/java/team/lala/timebank/config/SecurityConfig.java
@@ -1,63 +1,62 @@
 package team.lala.timebank.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import team.lala.timebank.service.TimeBankUserDetailsService;
+
 //@EnableWebSecurity //不知作用為何
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private TimeBankUserDetailsService timeBankUserDetailsService;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/home", "/js/**", "/css/**", "/img/**", "/vendor/**").permitAll()
-				.antMatchers("/admin/**").access("hasRole('ADMIN')")
-				.and().formLogin().loginPage("/login")
-				.failureHandler((req,res,exp)->{
-			         String errMsg="";
-			         if(exp instanceof BadCredentialsException ){
-			            errMsg="帳號或密碼錯誤";
-			         }else {
-			            errMsg="Unknown error - "+exp.getMessage();
-			         }
-			         req.setAttribute("message", errMsg);
-			         req.getRequestDispatcher("/WEB-INF/jsp/commons/login.jsp").forward(req, res);
-//			         req.getSession().setAttribute("message", errMsg);
-//			         res.sendRedirect("/login"); 
-			      })	
-				.and().logout()
-				.logoutSuccessUrl("/")
-				.deleteCookies("JSESSIONID") 
-				.and().csrf().disable();
+				.antMatchers("/admin/**").access("hasRole('ADMIN')").and().formLogin().loginPage("/login")
+				.failureHandler((req, res, exp) -> {
+					String errMsg = "";
+					if (exp instanceof BadCredentialsException) {
+						errMsg = "帳號或密碼錯誤";
+					} else {
+						errMsg = "Unknown error - " + exp.getMessage();
+					}
+					req.setAttribute("message", errMsg);
+					req.getRequestDispatcher("/WEB-INF/jsp/commons/login.jsp").forward(req, res);
+					// req.getSession().setAttribute("message", errMsg);
+					// res.sendRedirect("/login");
+				}).and().logout().logoutSuccessUrl("/").deleteCookies("JSESSIONID").and().csrf().disable();
 
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(timeBankUserDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+		auth.userDetailsService(timeBankUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 
-	//設定不需檢查的static檔案路徑，
-//	 @Override
-//	 public void configure(WebSecurity web) throws Exception {
-//		 web
-//		 .ignoring()
-//		 .antMatchers("/js/**", "/css/**", "/img/**", "/vendor/**"); 
-//	 }
-//	 
-	 
+	// 設定不需檢查的static檔案路徑，
+	// @Override
+	// public void configure(WebSecurity web) throws Exception {
+	// web
+	// .ignoring()
+	// .antMatchers("/js/**", "/css/**", "/img/**", "/vendor/**");
+	// }
+	//
 
-	
-	
-	
 }
--- src/main/java/team/lala/timebank/service/TimeBankUserDetailsService.java
+++ src/main/java/team/lala/timebank/service/TimeBankUserDetailsService.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import team.lala.timebank.config.SecurityUser;
 import team.lala.timebank.dao.MemberDao;
 import team.lala.timebank.entity.Member;
 
@@ -21,20 +20,19 @@ public class TimeBankUserDetailsService implements UserDetailsService {
 	@Override
 //	@Transactional(readOnly = true)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		//若帳號不存在，沒檢查會丟出InternalAuthenticationServiceException
-		//檢查後，SecurityConfig可用BadCredentialsException來判斷		
+
 		Member user = memberDao.findByAccount(username);
 		if (user == null) {
 			throw new UsernameNotFoundException("UsernameNotFound");
-		}
-//		user.getAuthorities().size();
-		
+		}		
 		System.out.println("user=" + user);
 		return user;
 		
 
 		
 	}
+	
+	
+	
 
 }
