49f9a9028ee3a562e63dbca0cdbde3be09706ed3
louis <louisfffflouisqqqq@gmail.com>
2018-12-24 09:57:02 +0800

    lombok

--- src/main/java/team/lala/timebank/entity/Penalty.java
+++ src/main/java/team/lala/timebank/entity/Penalty.java
@@ -12,6 +12,11 @@ import javax.persistence.Transient;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "PENALTY")
 public class Penalty {
@@ -54,80 +59,5 @@ public class Penalty {
 				+ penaltyTimeValue + "]";
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getOrderListId() {
-		return orderListId;
-	}
-
-	public void setOrderListId(Long orderListId) {
-		this.orderListId = orderListId;
-	}
-
-	public Long getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(Long memberId) {
-		this.memberId = memberId;
-	}
-
-	public Date getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(Date updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-
-	public Integer getPenaltyTimeValue() {
-		return penaltyTimeValue;
-	}
-
-	public void setPenaltyTimeValue(Integer penaltyTimeValue) {
-		this.penaltyTimeValue = penaltyTimeValue;
-	}
-
-	public Date getDateBefore() {
-		return dateBefore;
-	}
-
-	public void setDateBefore(Date dateBefore) {
-		this.dateBefore = dateBefore;
-	}
-
-	public Date getDateAfter() {
-		return dateAfter;
-	}
-
-	public void setDateAfter(Date dateAfter) {
-		this.dateAfter = dateAfter;
-	}
-
-
-
-	
 
 }
