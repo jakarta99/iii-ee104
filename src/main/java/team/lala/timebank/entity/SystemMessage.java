3c976e6af09121c2c155380218aad0a26c477406
Jasmine <jasminetseng0427@gmail.com>
2018-12-25 17:04:31 +0800

    add SystemMessage entity

---
+++ src/main/java/team/lala/timebank/entity/SystemMessage.java
@@ -0,0 +1,49 @@
+package team.lala.timebank.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import team.lala.timebank.enums.YesNo;
+
+@Getter
+@Setter
+@Entity
+@Table(name="SYSTEM_MESSAGE")
+public class SystemMessage {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(name="MEMBER",
+				referencedColumnName="id",  
+				insertable=false, updatable=false) 
+	private Member member;
+	
+	@Column(name="SENDER_ID")
+	private Long SenderId;
+	
+	
+	@Column(name="RELEASE_TIME")
+	private Date ReleaseTime;
+	
+	@Column(name="MESSAGE_TYPE")
+	private String MessageType;//之後改ENUM，還沒完全確定訊息類型種類(懲罰、核發時數通知、申訴結果)
+	
+	@Column(name="MESSAGE")
+	private String message;
+	
+	@Column(name="READ_STATUS")
+	private YesNo readStatus;
+
+}
