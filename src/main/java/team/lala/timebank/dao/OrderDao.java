005077a8edb9fa3258c59489db045fd736f74d86
陳宥任 <z403040473@gmail.com>
2018-12-25 17:34:22 +0800

    orderDao

--- src/main/java/team/lala/timebank/dao/OrderDao.java
+++ src/main/java/team/lala/timebank/dao/OrderDao.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 
+import team.lala.timebank.entity.Member;
 import team.lala.timebank.entity.Order;
-import team.lala.timebank.enums.YesNo;
 
 public interface OrderDao extends JpaRepository<Order,Long>, JpaSpecificationExecutor<Order>{
 
-	public List<Order> findByVolunteerId(Long volunteerId);
+	public List<Order> findByVolunteerId(Member volunteerId);
 	
 
 
