cde374968a9e5f2bdf909753f094746cb065919a
陳宥任 <z403040473@gmail.com>
2018-12-26 17:37:02 +0800

    VerifyController

--- src/main/java/team/lala/timebank/web/user/VolunteerVerifyController.java
+++ src/main/java/team/lala/timebank/web/user/VolunteerVerifyController.java
@@ -1,11 +1,11 @@
 package team.lala.timebank.web.user;
 
-import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,14 +18,14 @@ import team.lala.timebank.service.OrderService;
 @Slf4j
 @Controller
 @RequestMapping("/user/volunteerVerify")
-public class VolunteerVerify {
+public class VolunteerVerifyController {
 	
 	@Autowired
 	private OrderService orderService;
 	
 	@RequestMapping("/list")
-	public String listPage() {
-
+	public String listPage(@RequestParam(value = "id") Long id, Model model) {
+		model.addAttribute("MissionId",id);
 		return "/basic/user/volunteerRecruitment/volunteerVerify";
 	}
 
@@ -35,10 +35,10 @@ public class VolunteerVerify {
 			@RequestParam(value = "start", required = false) Optional<Integer> start,
 			@RequestParam(value = "length", required = false) Optional<Integer> length) {
 		int page = start.orElse(0) / length.orElse(10);
-		
+		log.debug("Mission={}", inputMission);
 //		Page<Mission> missions = missionService.findByAccount(principal, inputMission, PageRequest.of(page, length.orElse(10)));
 		Page<Order> orders = orderService.findByMission(inputMission, page, length);
-		
+		log.debug("Order={}", orders);
 		return orders;
 	}
 	
