95cd2645ad8b2e1cef371855467cefb86d10ba56
Sheila <sheila06048@gmail.com>
2018-12-31 22:13:37 +0800

    新增爬蟲網頁:Step30

--- src/main/java/team/lala/timebank/internationalVolunteer/projectsAbroad/ProjectsAbroadPageProcessor.java
+++ src/main/java/team/lala/timebank/internationalVolunteer/projectsAbroad/ProjectsAbroadPageProcessor.java
@@ -29,15 +29,15 @@ public class ProjectsAbroadPageProcessor implements PageProcessor {
 		
 //		判斷其頁面url是否match此url:/volunteer-projects/care/...
 		if (page.getUrl().regex("https://www.projects-abroad.com.tw/volunteer-projects/[^0-9]{4,40}/[^0-9]{5,40}/[^0-9]{4,30}/").match()) {
-			System.out.println("putField");
+			System.out.println("put field");
 			page.putField("organization", "Projects Abroad");
 			page.putField("orgUrl", "https://www.projects-abroad.com.tw/");
 			page.putField("title", page.getHtml().xpath("//*[@id=\"content\"]/article/h1/text()").toString());
-			page.putField("websiteUrl", page.getHtml().xpath("/html/head/meta[10]/@content").toString());
+			page.putField("websiteUrl", page.getUrl().toString());
 			page.putField("logo", page.getHtml().xpath("//*[@id=\"logo-top\"]").toString());
 			//取得div class='country-facts-box'內的標籤，以便取得此項目的資訊(如地點、志工角色、開始日期等)，並將資料放入field內
 			String countryFactsBox =  page.getHtml().xpath("//div[@class='country-facts-box']/ul/html()").toString();	        
-	        log.debug("countryFactsBox={}", countryFactsBox);
+//	        log.debug("countryFactsBox={}", countryFactsBox);
 			List<String> strongTextList = new Html(countryFactsBox).xpath("//li/strong/text()").all();
 	        List<String> spanTextList = new Html(countryFactsBox).xpath("//li/span/text()").all();
 	        for(int i = 0; i < strongTextList.size(); i++) {
@@ -49,27 +49,28 @@ public class ProjectsAbroadPageProcessor implements PageProcessor {
         			.xpath("//img/@src").regex("https://photos1.projects-abroad.com.tw/projects/.*").get().toString();
         	
         	page.putField("picture", imgUrl);
-        	System.out.println("put field end");
-		
+  		
 		}
 		else if (page.getUrl().regex("https://www.projects-abroad.com.tw/volunteer-projects/[^0-9]{4,30}/").match()) {		
+			System.out.println("add links");
 			page.addTargetRequests(
 					page.getHtml().css("div.box-project-row").links().all());		
 		}
 		// 列表頁 (加入新的url/頁面)
 		else {
+			System.out.println("項目url");
 			// 項目url	
 			String url = "https://www.projects-abroad.com.tw/volunteer-projects";
-			List<String> projectList = new ArrayList<>();
-			projectList.add(url+"/conservation-and-environment/");
-			projectList.add(url+"/sports/");
-			projectList.add(url+"/building/");
-			projectList.add(url+"/archaeology/");
-			projectList.add(url+"/agriculture-and-farming/");
-			projectList.add(url+"/culture-and-community/");
-			projectList.add(url+"/creative-and-performing-arts/");		
-			page.addTargetRequests(projectList);
-			
+			List<String> targetRequest = new ArrayList<>();
+			targetRequest.add(url+"/conservation-and-environment/");
+			targetRequest.add(url+"/sports/");
+			targetRequest.add(url+"/building/");
+			targetRequest.add(url+"/archaeology/");
+			targetRequest.add(url+"/agriculture-and-farming/");
+			targetRequest.add(url+"/culture-and-community/");
+			targetRequest.add(url+"/creative-and-performing-arts/");		
+			page.addTargetRequests(targetRequest);
+			System.out.println(page.getTargetRequests());
 			
 		}
 		
