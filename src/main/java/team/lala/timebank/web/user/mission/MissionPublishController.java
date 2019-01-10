ac7f678a94460ae607e36a44f24ac4cac2c9d452
陳宥任 <z403040473@gmail.com>
2019-01-10 16:52:15 +0800

    missionadd.jsp missionedit.jsp

--- src/main/java/team/lala/timebank/web/user/mission/MissionPublishController.java
+++ src/main/java/team/lala/timebank/web/user/mission/MissionPublishController.java
@@ -13,13 +13,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
 import team.lala.timebank.commons.ajax.AjaxResponse;
+import team.lala.timebank.entity.Member;
 import team.lala.timebank.entity.Mission;
 import team.lala.timebank.entity.ServiceType;
+import team.lala.timebank.service.MemberService;
 import team.lala.timebank.service.MissionService;
 import team.lala.timebank.service.ServiceTypeService;
 @Slf4j
@@ -28,7 +29,8 @@ import team.lala.timebank.service.ServiceTypeService;
 public class MissionPublishController {
 	@Autowired
 	private MissionService missionService;
-
+	@Autowired
+	private MemberService memberService;
 	@Autowired
 	private ServiceTypeService serviceTypeService;
 
@@ -40,14 +42,36 @@ public class MissionPublishController {
 
 		return "/basic/user/volunteerRecruitment/mission_add";
 	}
-
+	//刊登mission
 	@RequestMapping(value = "/insert", method = RequestMethod.POST)
 	public String insert(Mission mission, Principal principal,
 			@RequestParam("missionPicture") MultipartFile missionPicture, HttpServletRequest request, Model model) {
 		AjaxResponse<Mission> response = new AjaxResponse<Mission>();
-		try {
+		Member member = memberService.findByAccount(principal.getName());
+		List<ServiceType> serviceType = serviceTypeService.findAll();
+		model.addAttribute("serviceType", serviceType);
+		model.addAttribute("mission", mission);
+		model.addAttribute(response);
+		log.debug("member.getBalanceValue()={}",member.getBalanceValue());
+		
+		if(member.getBalanceValue() == null || mission.getTimeValue() == null) {
+			member.setBalanceValue(0);
+			mission.setTimeValue(0);
+			log.debug("member.getBalanceValue()={}",member.getBalanceValue());
+			return "/basic/user/volunteerRecruitment/mission_add";
+		}else if(member.getBalanceValue().intValue() == 0 || mission.getTimeValue().intValue() == 0) {
+			response.addMessage("餘額不足 無法刊登");
+			return "/basic/user/volunteerRecruitment/mission_add";
+		}
+		//使用額度超過member餘額 無法刊登
+		if(member.getBalanceValue().intValue() >= mission.getTimeValue().intValue()) {
+			//member可使用額度減少
+			member.setBalanceValue(member.getBalanceValue().intValue() - mission.getTimeValue().intValue());
 			missionService.insert(mission, principal);
-			model.addAttribute(response);
+		}else {
+			return "/basic/user/volunteerRecruitment/mission_add";
+		}
+		try {	
 			if (missionPicture.getOriginalFilename().length() > 0) {
 				// 取得應用程式根目錄中圖片之路徑
 				String realPath = request.getServletContext().getRealPath("/") + "WEB-INF\\image\\user\\mission\\";
@@ -59,23 +83,19 @@ public class MissionPublishController {
 				}
 				// 檔名
 				String location = realPath + "missionPicture_" + mission.getId() + ".jpg";
-
 				// 寫出檔案到Server
 				FileOutputStream fos = new FileOutputStream(location);
 				fos.write(missionPicture.getBytes());
 				fos.close();
-
 				// 將檔名存入DB
 				mission.setMissionPicName("missionPicture_" + mission.getId() + ".jpg");
 				missionService.insert(mission, principal);
-
 			}
 		} catch (Exception e) {
-			response.addMessage("新增失敗，" + e.getMessage());
+			response.addMessage("刊登失敗，" + e.getMessage());
 			e.printStackTrace();
-			model.addAttribute(response);
 		}
-		return "/basic/user/volunteerRecruitment/mission_list";
+		return "/basic/user/volunteerRecruitment/mission_add";
 	}
 
 }
