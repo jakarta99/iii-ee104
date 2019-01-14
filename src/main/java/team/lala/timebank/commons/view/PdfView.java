62b1f34e515e4de1af242473ff1f017654b080d5
Jasmine <jasminetseng0427@gmail.com>
2019-01-14 11:24:16 +0800

    PDF基本款文字浮水印

--- src/main/java/team/lala/timebank/commons/view/PdfView.java
+++ src/main/java/team/lala/timebank/commons/view/PdfView.java
@@ -1,5 +1,6 @@
 package team.lala.timebank.commons.view;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -8,21 +9,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfGState;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class PdfView extends AbstractITextPdfView {
 
-
+	
 
 	 @Override
-	    protected void buildPdfDocument(Map<String, Object> pdfModel, Document doc,
+	 protected void buildPdfDocument(Map<String, Object> pdfModel, Document doc,
 	            PdfWriter writer, HttpServletRequest request, HttpServletResponse response)
 	            throws Exception {
 	        //取得Controller塞進來的資料(sheetname、表頭、資料內容)
@@ -68,10 +75,63 @@ public class PdfView extends AbstractITextPdfView {
 				}
 			}
 	        
+	        //浮水印
+	        writer.setPageEvent(new WatermarkPageEvent());
+	        
+	        
+	        
 	        //把表格寫入檔案中
 	        doc.add(table);
+	        
 	         
 	    }
 	
-
+	 	
+	 public void addWatermark(PdfStamper pdfStamper, String waterMarkName) {
+			PdfContentByte content = null;
+			BaseFont base = null;
+			Rectangle pageRect = null;
+			PdfGState gs = new PdfGState();
+			try {
+				// 设置字体
+				base = BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+			} catch (DocumentException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (base == null || pdfStamper == null) {
+					return;
+				}
+				// 设置透明度为0.4
+				gs.setFillOpacity(0.4f);
+				gs.setStrokeOpacity(0.4f);
+				int toPage = pdfStamper.getReader().getNumberOfPages();
+				for (int i = 1; i <= toPage; i++) {
+					pageRect = pdfStamper.getReader().getPageSizeWithRotation(i);
+					// 计算水印X,Y坐标
+					float x = pageRect.getWidth() / 2;
+					float y = pageRect.getHeight() / 2;
+					// 获得PDF最顶层
+					content = pdfStamper.getOverContent(i);
+					content.saveState();
+					// set Transparency
+					content.setGState(gs);
+					content.beginText();
+					content.setColorFill(BaseColor.GRAY);
+					content.setFontAndSize(base, 60);
+					// 水印文字成45度角倾斜
+					content.showTextAligned(Element.ALIGN_CENTER, waterMarkName, x, y, 45);
+					content.endText();
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			} finally {
+				content = null;
+				base = null;
+				pageRect = null;
+			}
+		}
+	 
 }
