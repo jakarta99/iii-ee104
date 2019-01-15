06f386a7a7b0ed28af7090299dd4e1f06773fdd7
Jasmine <jasminetseng0427@gmail.com>
2019-01-15 12:05:03 +0800

    捐款統計圖表調整

--- src/main/java/team/lala/timebank/commons/view/PdfViewForChart.java
+++ src/main/java/team/lala/timebank/commons/view/PdfViewForChart.java
@@ -1,6 +1,10 @@
 package team.lala.timebank.commons.view;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +20,7 @@ import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfGState;
@@ -24,7 +29,7 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
 
-public class PdfView extends AbstractITextPdfView {
+public class PdfViewForChart extends AbstractITextPdfView {
 
 	
 
@@ -32,48 +37,34 @@ public class PdfView extends AbstractITextPdfView {
 	 protected void buildPdfDocument(Map<String, Object> pdfModel, Document doc,
 	            PdfWriter writer, HttpServletRequest request, HttpServletResponse response)
 	            throws Exception {
-	        //取得Controller塞進來的資料(sheetname、表頭、資料內容)
-		 	String title = (String) pdfModel.get("title");
-		 	List<String> headers = (List<String>) pdfModel.get("headers");
-			List<List<String>> results = (List<List<String>>) pdfModel.get("results");
+	        //取得Controller塞進來的資料(htmlStr)
+		 	String htmlStr = (String) pdfModel.get("htmlStr");
+//		 	List<String> headers = (List<String>) pdfModel.get("headers");
+//			List<List<String>> results = (List<List<String>>) pdfModel.get("results");
 
 			//放標題
-	        doc.add(new Paragraph(title));
+	        doc.add(new Paragraph("捐款統計表"));
 	         
-	        //在PDF中創造一個table，並設置欄位數量
-	        PdfPTable table = new PdfPTable(headers.size());
-	        table.setWidthPercentage(100.0f);
-//	        table.setWidths(new float[] {3.0f, 2.0f, 2.0f, 2.0f, 1.0f}); //
-	        table.setSpacingBefore(10);
-	         
-	        // 設計表頭字體顏色
-	        Font font = FontFactory.getFont(FontFactory.HELVETICA);
-	        font.setColor(BaseColor.WHITE);
-	         
-	        // 設計表頭底色
-	        PdfPCell cell = new PdfPCell();
-	        cell.setBackgroundColor(BaseColor.GRAY);
-	        cell.setPadding(headers.size());
-	         
-	        // 塞入表頭資料
-	        for(String header : headers) {
-	        	cell.setPhrase(new Phrase(header, font)); //指定塞入資料的樣式
-		        table.addCell(cell);
-	        }
+	        try {
+//			    String k = "<html>"+ htmlStr +"</html>";
+			    String k = "<html><body> This is my Project </body></html>";
+
+			    doc.open();
+			    HTMLWorker htmlWorker = new HTMLWorker(doc);
+			    htmlWorker.parse(new StringReader(k));
+			    doc.close();
+
+			} catch (Exception e) {
+			    e.printStackTrace();
+			}
+	        
 	        
 	        
 	        //塞入資料
 	        //如有中文，必須指明中文字體	        
 	        BaseFont bfChinese = BaseFont.createFont("C:\\WINDOWS\\Fonts\\KAIU.TTF", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
 	        
-	        Font chineseFont = new Font(bfChinese, 12, Font.NORMAL ); 
-	        PdfPCell contentCell = new PdfPCell();
-	        for (List<String> result : results) {
-				for (String value : result) {
-					contentCell.setPhrase(new Phrase(value, chineseFont));
-					table.addCell(contentCell); 
-				}
-			}
+
 	        
 	        //浮水印
 	        writer.setPageEvent(new WatermarkPageEvent());
@@ -81,7 +72,7 @@ public class PdfView extends AbstractITextPdfView {
 	        
 	        
 	        //把表格寫入檔案中
-	        doc.add(table);
+//	        doc.add(table);
 	        
 	         
 	    }
