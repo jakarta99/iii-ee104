67d34ac9c11c2383b6f264e2b56e4b05f62aee83
louis <louisfffflouisqqqq@gmail.com>
2019-01-17 18:02:25 +0800

    import

--- src/main/java/team/lala/timebank/web/commons/DomesticVolunteerController.java
+++ src/main/java/team/lala/timebank/web/commons/DomesticVolunteerController.java
@@ -12,11 +12,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.microsoft.sqlserver.jdbc.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import team.lala.timebank.entity.Member;
--- src/main/java/team/lala/timebank/web/commons/InternationalVolunteerController.java
+++ src/main/java/team/lala/timebank/web/commons/InternationalVolunteerController.java
@@ -7,15 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.microsoft.sqlserver.jdbc.StringUtils;
-
 import lombok.extern.slf4j.Slf4j;
 import team.lala.timebank.entity.InternationalVolunteer;
-import team.lala.timebank.entity.Mission;
 import team.lala.timebank.entity.MyCollection;
 import team.lala.timebank.enums.MyCollectionType;
 import team.lala.timebank.enums.YesNo;
