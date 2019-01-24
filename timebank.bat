40e41022690204a7efc2a8ae66a826647fd95a1b
Brian <Brian@LAPTOP-19936VFA>
2019-01-24 19:06:48 +0800

    bat

---
+++ timebank.bat
@@ -0,0 +1,1 @@
+java -jar target\iii-ee104-0.0.1-SNAPSHOT.jar --spring.datasource.url=jdbc:sqlserver://localhost:1433;databaseName=timebank1234 --spring.datasource.user=xxxx --spring.datasource.password=pass@@@
